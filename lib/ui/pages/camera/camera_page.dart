ddcab79b82a51344efe002730145f6bfa1028966
David Reyes <reyisacc@gmail.com>
2022-12-25 17:52:01 -0400

    camera_page

--- lib/ui/pages/camera/camera_page.dart
+++ lib/ui/pages/camera/camera_page.dart
@@ -58,89 +58,45 @@ class _CameraPageState extends State<CameraPage> {
           bottomNavigationBar: Container(
             height: 161,
             color: AppColors.vulcan,
-            child: Stack(
-              children: [
-                LayoutBuilder(
-                  builder: (BuildContext context, BoxConstraints constraints) {
-                    var _height = constraints.maxHeight;
-                    var _widthItem = constraints.maxWidth / 5;
-                    var _paddingItem = (_widthItem - 85) / 2;
-                    print("alto: $_height  ancho: $_widthItem  ");
+            child: BottomNavigationBar(
+              currentIndex: _selectedIndex,
+              type: BottomNavigationBarType.fixed,
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              //  Color.fromARGB(250, 20, 18, 32),
 
-                    return Padding(
-                      padding: EdgeInsets.only(
-                        left: _widthItem * _selectedIndex + _paddingItem,
-                      ), //
-                      child: Stack(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(bottom: 45),
-                            width: 85,
-                            decoration: BoxDecoration(
-                                color: Colors.white, shape: BoxShape.circle),
-                          ),
-                          Container(
-                            width: 85,
-                            margin: EdgeInsets.only(bottom: 100),
-                            decoration: BoxDecoration(
-                                color: Colors.white, shape: BoxShape.rectangle),
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(top: 150),
-                            width: 85,
-                            height: 5,
-                            color: Colors.white,
-                          ),
-                        ],
-                      ),
-                    );
-                  },
+              selectedFontSize: sclH(context) * 3,
+              selectedItemColor: AppColors.royalBlue,
+              selectedIconTheme: IconThemeData(size: sclH(context) * 6),
+              unselectedFontSize: sclH(context) * 2,
+              unselectedItemColor: Colors.white,
+              unselectedIconTheme: IconThemeData(size: sclH(context) * 3),
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.image_outlined),
+                  label: 'Fondo',
                 ),
-                Container(
-                  height: 3,
-                  color: Colors.white,
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.filter_b_and_w),
+                  label: 'Filtro',
                 ),
-                BottomNavigationBar(
-                  currentIndex: _selectedIndex,
-                  type: BottomNavigationBarType.fixed,
-                  elevation: 0,
-                  backgroundColor: Colors.transparent,
-                  //  Color.fromARGB(250, 20, 18, 32),
-
-                  selectedFontSize: sclH(context) * 3,
-                  selectedItemColor: AppColors.royalBlue,
-                  selectedIconTheme: IconThemeData(size: sclH(context) * 6),
-                  unselectedFontSize: sclH(context) * 2,
-                  unselectedItemColor: Colors.white,
-                  unselectedIconTheme: IconThemeData(size: sclH(context) * 3),
-                  items: const [
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.image_outlined),
-                      label: 'Fondo',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.filter_b_and_w),
-                      label: 'Filtro',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.video_camera_back_outlined),
-                      label: 'Video',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.camera_enhance_sharp),
-                      label: 'Efecto',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.settings),
-                      label: 'Ajustes',
-                    ),
-                  ],
-                  onTap: (value) {
-                    _selectedIndex = value;
-                    setState(() {});
-                  },
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.video_camera_back_outlined),
+                  label: 'Video',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.camera_enhance_sharp),
+                  label: 'Efecto',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.settings),
+                  label: 'Ajustes',
                 ),
               ],
+              onTap: (value) {
+                _selectedIndex = value;
+                setState(() {});
+              },
             ),
           ),
         ));
