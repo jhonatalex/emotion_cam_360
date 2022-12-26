b8330ba017da6b1dab3084411608447c64192941
David Reyes <reyisacc@gmail.com>
2022-12-26 19:39:42 -0400

    push del merge

--- lib/ui/routes/route_names.dart
+++ lib/ui/routes/route_names.dart
@@ -4,9 +4,9 @@ class RouteNames {
   static const String home = '/home';
   static const String menu = '/menu';
   static const String camera = '/camera';
+  static const String finishQr = '/finish-qr';
   static const String finishqr = '/finishqr';
   static const String cameraScreen = '/camera-screen';
-  static const String finishQr = '/finish-qr';
   static const String videoScreen = '/video-screen';
   static const String uploadVideo = '/upload-video';
 }
