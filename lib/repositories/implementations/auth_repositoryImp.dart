23caf99dc5a3b9d696c0e6c33109ff4c6ea5eb30
JhonatalexZenta <jhonatan.mejias@zentagroup.com>
2023-03-16 23:09:13 -0300

    fix ggogle sha1 up version ggogle

--- lib/repositories/implementations/auth_repositoryImp.dart
+++ lib/repositories/implementations/auth_repositoryImp.dart
@@ -77,7 +77,7 @@ class AuthRepositoryImp implements AuthRepository {
   final GoogleSignIn _googleSignIn = GoogleSignIn(
     scopes: [
       'email',
-      //'https://www.googleapis.com/auth/contacts.readonly',
+      'https://www.googleapis.com/auth/contacts.readonly',
     ],
   );
 
