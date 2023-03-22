8ac3d3749d35db0c7bc10c9032c60b4f88f6ec8e
David Reyes <reyisacc@gmail.com>
2023-03-21 22:56:11 -0400

    subs fixed

--- lib/ui/widgets/messenger_snackbar.dart
+++ lib/ui/widgets/messenger_snackbar.dart
@@ -12,41 +12,49 @@ MessengerSnackBar(context, text) {
       children: [
         Container(
           width: sclW(context) * 100,
-          padding: EdgeInsets.all(20),
-          margin: EdgeInsets.only(top: 20),
-          child: Text(
-            text,
-            style: const TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-            ),
+          padding: const EdgeInsets.all(20),
+          // margin: EdgeInsets.only(top: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(left: 20),
+                child: Container(
+                  height: sclH(context) * 4,
+                  width: sclH(context) * 4,
+                  child: Center(
+                    child: Text(
+                      "!",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: sclH(context) * 3.5,
+                      ),
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black38, width: 0.5),
+                      color: AppColors.violet,
+                      borderRadius: BorderRadius.circular(50)),
+                ),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Text(
+                text,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ],
           ),
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             border: Border(
               top: BorderSide(color: AppColors.violet, width: 2),
             ),
             color: AppColors.vulcan,
-          ),
-        ),
-        Container(
-          margin: EdgeInsets.only(left: 20),
-          child: Container(
-            height: sclH(context) * 3,
-            width: sclH(context) * 3,
-            child: Center(
-              child: Text(
-                "!",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: sclH(context) * 2.5,
-                ),
-              ),
-            ),
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black38, width: 0.5),
-                color: AppColors.violet,
-                borderRadius: BorderRadius.circular(50)),
           ),
         ),
       ],
